32df5371044a64ff511b7f41643ab6826db54b0c
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-31 11:40:33 +0100

    add some more defaults

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 // Place any global data in this file.
 
-export const SITE_TITLE = "Elijah James And The Nightmares | Official Website";
+export const SITE_TITLE = 'Elijah James And The Nightmares | Official Website';
+export const SITE_DESCRIPTION =
+  'Elijah James And The Nightmares are an indie-alternative-folk band from Liverpool, United Kindom.';
