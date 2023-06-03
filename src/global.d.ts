eb0d4380b954017795bd5c949af52edb0ad55edb
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-03 22:09:52 +0100

    add global types

---
+++ src/global.d.ts
@@ -0,0 +1,26 @@
+declare type Credits = {
+  writtenBy: string;
+  arrangedBy: string;
+  producedBy: string;
+  mixedBy: string;
+  masteredBy: string;
+  recordedAt: string[];
+  personel: string[];
+  albumArtDesignedBy: string;
+  albumArtBy: string;
+};
+
+declare type Link = {
+  spotify?: string;
+  bandcamp?: string;
+};
+
+declare type Album = {
+  img: string;
+  title: string;
+  releaseDate: string;
+  tracklist: Object;
+  about: string;
+  credits: Credits;
+  links: Link;
+};
