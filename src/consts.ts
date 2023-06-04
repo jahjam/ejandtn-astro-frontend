080e4561bddaee564bf99b4ec69d7b6b9a52cc26
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-04 22:05:15 +0100

    add new consts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 export const SITE_TITLE = 'Elijah James And The Nightmares | Official Website';
 export const SITE_DESCRIPTION =
   'Elijah James And The Nightmares are an indie-alternative-folk band from Liverpool, United Kindom.';
+
+export const SUBSCRIBE_TITLE = 'Subscribe to EJANDTN';
+export const SUBSCRIBE_DESCRIPTION =
+  "Subcribe to EJANDTN's newsletter for regular updates regarding new music, new gigs, and new merch!";
