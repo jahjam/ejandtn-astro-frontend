c9a39891ac35f21508f8428aa1505ebdfd4cb6fa
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-04 12:16:39 +0100

    add video section

---
+++ src/data/videos.ts
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    id: 1,
+    title: "Because I'm A Giant",
+    url: '/src/assets/youtube-art/BIAG-thumb-reduce.jpg',
+    src: 'https://www.youtube.com/embed/EEPCzYCF-pU?enablejsapi=1&version=3&playerapiid=ytplayer',
+  },
+  {
+    id: 2,
+    title: "It's Too Late Now To Say: Two Songs EP",
+    url: '/src/assets/youtube-art/EP-thumb-reduce.jpg',
+    src: 'https://www.youtube.com/embed/CMTG1q8EZ-M?enablejsapi=1&version=3&playerapiid=ytplayer',
+  },
+  {
+    id: 3,
+    title: 'I Hate It Here But You Are Here And I Love You Single',
+    url: '/src/assets/youtube-art/hate-it-here-thumb-reduce.jpg',
+    src: 'https://www.youtube.com/embed/6K1YG8RefHo?enablejsapi=1&version=3&playerapiid=ytplayer',
+  },
+  {
+    id: 4,
+    title: 'On A Stage Single',
+    url: '/src/assets/youtube-art/on-a-stage-thumb-reduce.jpg',
+    src: 'https://www.youtube.com/embed/y65x-Zlhom8?enablejsapi=1&version=3&playerapiid=ytplayer',
+  },
+];
