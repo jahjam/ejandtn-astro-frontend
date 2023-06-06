d969dd857fbf5a9c9c4787a1495404e0a9ff5510
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-06 15:48:33 +0100

    add videos

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,24 +1,36 @@
 export const videos = [
   {
-    id: 1,
+    id: 6,
+    title: "The Coroner's Report Single",
+    url: '/src/assets/youtube-art/coroners-report-thumb.png',
+    src: 'https://www.youtube.com/watch?v=N_la7QV3F9o',
+  },
+  {
+    id: 5,
+    title: 'Man In The Diner Single',
+    url: '/src/assets/youtube-art/man-in-the-diner-thumb.png',
+    src: 'https://www.youtube.com/watch?v=hVJwG3kjplA',
+  },
+  {
+    id: 4,
     title: "Because I'm A Giant",
     url: '/src/assets/youtube-art/BIAG-thumb-reduce.jpg',
     src: 'https://www.youtube.com/embed/EEPCzYCF-pU?enablejsapi=1&version=3&playerapiid=ytplayer',
   },
   {
-    id: 2,
+    id: 3,
     title: "It's Too Late Now To Say: Two Songs EP",
     url: '/src/assets/youtube-art/EP-thumb-reduce.jpg',
     src: 'https://www.youtube.com/embed/CMTG1q8EZ-M?enablejsapi=1&version=3&playerapiid=ytplayer',
   },
   {
-    id: 3,
+    id: 2,
     title: 'I Hate It Here But You Are Here And I Love You Single',
     url: '/src/assets/youtube-art/hate-it-here-thumb-reduce.jpg',
     src: 'https://www.youtube.com/embed/6K1YG8RefHo?enablejsapi=1&version=3&playerapiid=ytplayer',
   },
   {
-    id: 4,
+    id: 1,
     title: 'On A Stage Single',
     url: '/src/assets/youtube-art/on-a-stage-thumb-reduce.jpg',
     src: 'https://www.youtube.com/embed/y65x-Zlhom8?enablejsapi=1&version=3&playerapiid=ytplayer',
