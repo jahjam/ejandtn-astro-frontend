af0a2a2da6092e9cc9e0b551125c621f026b4225
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-13 09:26:04 +0100

    add consts for new page

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,9 @@ export const SITE_DESCRIPTION =
 export const SUBSCRIBE_TITLE = 'Subscribe to EJANDTN';
 export const SUBSCRIBE_DESCRIPTION =
   "Subcribe to EJANDTN's newsletter for regular updates regarding new music, new gigs, and new merch!";
+
+export const DAY_TICKETS_TITLE = 'Day Tickets';
+export const DAY_TICKETS_DESCRIPTION =
+  "I don't like to think of day tickets as poems, because poems are sacred, and these are throwaway. Each one contains a single line that never made it into a song, from which I have formed short passages of text. I call them day tickets as I often find myself writing them to provide me safe passage through a dreadful day, of which there are more than I'd like. Below is the most recent dreadful day in which I could muster the strength to write.";
+export const DAY_TICKETS_ARCHIVE_DESCRIPTION =
+  'Welcome to the archive! Here you will find all the day tickets ever published.';
