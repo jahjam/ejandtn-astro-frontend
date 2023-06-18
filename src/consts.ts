02bd63bfda992625f33555029259aeb4cc1b6457
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-06-18 17:40:58 +0100

    add consts for pages

--- src/consts.ts
+++ src/consts.ts
@@ -2,14 +2,29 @@
 
 export const SITE_TITLE = 'Elijah James And The Nightmares | Official Website';
 export const SITE_DESCRIPTION =
-  'Elijah James And The Nightmares are an indie-alternative-folk band from Liverpool, United Kindom.';
+  'The official website for Elijah James And The Nightmares, an indie-alternative-folk band from Liverpool, United Kindom.';
 
 export const SUBSCRIBE_TITLE = 'Subscribe to EJANDTN';
 export const SUBSCRIBE_DESCRIPTION =
   "Subcribe to EJANDTN's newsletter for regular updates regarding new music, new gigs, and new merch!";
 
-export const DAY_TICKETS_TITLE = 'Day Tickets';
+export const DAY_TICKETS_TITLE =
+  'Elijah James And The Nightmares | Day Tickets';
 export const DAY_TICKETS_DESCRIPTION =
   "I don't like to think of day tickets as poems, because poems are sacred, and these are throwaway. Each one contains a single line that never made it into a song, from which I have formed short passages of text. I call them day tickets as I often find myself writing them to provide me safe passage through a dreadful day, of which there are more than I'd like. Below is the most recent dreadful day in which I could muster the strength to write.";
 export const DAY_TICKETS_ARCHIVE_DESCRIPTION =
   'Welcome to the archive! Here you will find all the day tickets ever published.';
+
+export const BLOG_TITLE = 'Elijah James And The Nightmares | Blog';
+export const BLOG_DESCRIPTION =
+  "Welcome to Elijah James And The Nightmares personal blog. Here you will find the most recent updates from the band that can only be found here or via our mailing list if you're subscribed.";
+
+export const PHOTOGRAPHY_TITLE =
+  'Elijah James And The Nightmares | Animals & Architecture';
+export const PHOTOGRAPHY_DESCRIPTION =
+  "Welcome to Animals & Architecture. Here you will find all of Elijah James's street photography in no particular order.";
+
+export const SOUNDSCAPES_TITLE =
+  "Elijah James And The Nightmares | Eli's Little Dictaphone";
+export const SOUNDSCAPES_DESCRIPTION =
+  "Welcome to Eli's Little Dictaphone. Here you will find daily soundscapes recorded by Elijah James.";
