ba17063f06f48ce5debeaf6d34de0d23d4c5837a
jahjam <elijahjamesuk@hotmail.com>
2025-02-17 23:59:37 +0000

    loads of changes what

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -50,3 +50,26 @@ export const videos = [
     src: 'https://www.youtube.com/watch?v=GIX5lhbnidU',
   },
 ];
+
+export const videoData = new Map<string, string>([
+  [
+    'On A Stage',
+    "I raised the money for this video by putting on a band 'launch' gig in District in Liverpool that ended up selling super well and being a massive success. We'd practiced as a band for a solid three months at that point and were the tightest we'd ever been and ever would be again.",
+  ],
+  [
+    'I Hate It Here, But You Are Here, And I Love You',
+    "We set out to make a one take video driving around Liverpool whilst I wrote the lyrics on the window. We did five takes in total but the first one (the one you can view now) was the only one where we didn't get caught it traffic and ended up at the docks. It's not perfect and I spell 'vain' wrong (I crumbled under the 'one take' pressure) but it's a cool little video that reminds me of the ramshackle making of this album.",
+  ],
+  [
+    'Movie In Your Eyes',
+    "Filmed in a warehouse in the Baltic area of Liverpool, I had a very loose concept and we did our best to capture an interesting music video in the short time we had there. I reached out to an artist called Sev Coursen (https://sevcoursen.com/) to use footage that he'd filmed in the background and he graciously granted me permission. There's a lot of things I would do differently now, if I could do it again. Alas, there's a lot about Because I'm A Giant that I would do again too, so for that reason, it works.",
+  ],
+  [
+    "There's Everything Else And Then There's You",
+    "Filmed in a flat I was living in in Liverpool. It's pretty self-explanatory really. All I will say is I wish I had a stylist.",
+  ],
+  [
+    "It's Too Late Now To Say: Two Songs",
+    "This is my favourite video I've filmed to date. The projected images are stock footage that I assembled into a loose narrative and then layered the lyrics on top of. I was say in front of the project playing the songs through so that my shadow distorted the projection. What you can't see is the projector and camera are balanced precariously on loads of cushions. It was filmed on an IPad and in the same flat as the video for There's Everything Else And Then There's You. Not long after filming this one, we moved out of there because the upstairs neighbour had super loud parties and threw a gigantic flesh coloured dildo onto this secluded, inaccessible balcony thing right in front of our living room window. So for the remaining few months, everytime we looked out of our window, we saw that. Unfortunately, the next flat we moved into wasn't much better, but that's a tale for another time.",
+  ],
+]);
