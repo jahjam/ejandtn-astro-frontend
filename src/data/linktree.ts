48fdc96d58e24eca939444d70fd215c7541ba963
atomicmammothstudios <atomicmammothstudios@gmail.com>
2026-06-29 15:21:00 +0100

    update links and add linktree

---
+++ src/data/linktree.ts
@@ -0,0 +1,14 @@
+export const linkTree = [
+  {
+    title: 'That Girl Of Mine - Live Solo Video',
+    link: 'https://youtu.be/HxfpDsXT5U0?si=yhrpejI5E_6iBcGp',
+  },
+  {
+    title: 'Hellish Bending Towards The Light - OUT NOW!!',
+    link: 'https://ejhallows.bandcamp.com/album/the-hellish-bending-towards-the-light',
+  },
+  {
+    title: 'Subscribe To Newsletter',
+    link: 'https://ejhallows.com/subscribe/',
+  },
+];
